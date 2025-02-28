c8646961025d076e6b3e36d1e295d3ab7d430bce
joeyproxxl <68194340+joeyproxxl@users.noreply.github.com>
2025-02-28 16:54:08 +0100

    oppakken script verbeter

--- Abandoned Worker/Assets/Scripts/Oppakken.cs
+++ Abandoned Worker/Assets/Scripts/Oppakken.cs
@@ -1,62 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPickup : MonoBehaviour
+public class PickUpBlock : MonoBehaviour
 {
-    public Transform holdPosition; // Position where the object will be held
-    public float pickupRange = 3f; // Distance at which the player can pick up objects
-    public LayerMask pickupLayer; // Layer for pickable objects
-    public KeyCode pickupKey = KeyCode.E; // Key to pick up and drop objects
+    public GameObject pickUpText;
+    public GameObject BlockOnPlayer;
 
-    private Rigidbody heldObject;
-
-    void Update()
+    void Start()
     {
-        if (Input.GetKeyDown(pickupKey))
-        {
-            if (heldObject == null)
-            {
-                TryPickup();
-            }
-            else
-            {
-                DropObject();
-            }
-        }
-
-        if (heldObject != null)
-        {
-            MoveHeldObject();
-        }
+        pickUpText.SetActive(false);
     }
 
-    void TryPickup()
+    private void OnTriggerStay(Collider other)
     {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, pickupRange, pickupLayer))
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (hit.collider.gameObject.TryGetComponent(out Rigidbody rb))
+            pickUpText.SetActive(true);
+
+            // Check if 'E' key is held down to pick up the Block
+            if (Input.GetKey(KeyCode.E))
             {
-                heldObject = rb;
-                heldObject.useGravity = false;
-                heldObject.freezeRotation = true;
+                gameObject.SetActive(false);
+                BlockOnPlayer.SetActive(true);
+                pickUpText.SetActive(false);
             }
         }
     }
 
-    void MoveHeldObject()
+    private void OnTriggerExit(Collider other)
     {
-        Vector3 targetPosition = holdPosition.position;
-        Vector3 direction = (targetPosition - heldObject.position) * 10f;
-        heldObject.velocity = direction;
-    }
-
-    void DropObject()
-    {
-        if (heldObject != null)
+        if (other.gameObject.CompareTag("Player"))
         {
-            heldObject.useGravity = true;
-            heldObject.freezeRotation = false;
-            heldObject = null;
+            pickUpText.SetActive(false);
         }
     }
 }
